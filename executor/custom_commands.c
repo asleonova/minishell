7170801eb0487c6399a1910b21a6b98301efd6ee
Deane Bliss <dbliss@mi-a3.msk.21-school.ru>
2020-11-27 14:18:57 +0300

    echo done

--- executor/custom_commands.c
+++ executor/custom_commands.c
@@ -5,38 +5,52 @@
 
 int ft_echo(t_commands *command)
 {
+    int flag;
+    flag = 0;
+
     if (command->count_args == 0)
     {
         write(1, "\n", 1);
         return(SUCCESS);
     }
-    while(command->lst) // надо чекнуть как по листам правильно двигаться
+    while(command->lst)
     {
         if (ft_strcmp("-n", command->lst->content) == 0)
-            command->lst = command->lst->next;
-        ft_putstr_fd(command->lst->content, 1);
+        {
+            if(command->lst->next != NULL)
+                command->lst = command->lst->next;
+            flag = 1;
+        }
+        if (ft_strcmp("-n", command->lst->content) != 0)
+            ft_putstr_fd(command->lst->content, 1);
         if (command->lst->next != NULL)
             write(1, " ", 1);
         command->lst = command->lst->next;
     }
-    if (ft_strcmp("-n", command->lst->content) != 0)
+    if (flag == 0)
         write(1, "\n", 1);
     return(SUCCESS); 
 }
 
-int main()
+void echo_check()
 {
     t_commands command;
     command.lst = NULL;
     command.lst = (t_list *) malloc(sizeof(t_list));
     command.lst->content = "hello";
-    command.lst->next = NULL;
-    //command.lst->content = "-n";
-    //command.lst->next = (t_list *) malloc(sizeof(t_list));
-    //command.lst->content = "hello";
-    //command.lst->next->next = NULL;
-    //command.cmd = "echo";
+    // command.lst->next = NULL;
+    command.lst->next = (t_list *) malloc(sizeof(t_list));
+    command.lst->next->content = "hello";
+    command.lst->next->next = (t_list *) malloc(sizeof(t_list));
+    command.lst->next->next->content = "world"; 
+    command.lst->next->next->next = NULL;
+    command.cmd = "echo";
     command.count_args = 1;
     ft_echo(&command);
+}
+
+int main()
+{
+    echo_check();
     return (0);
 }
