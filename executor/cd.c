b6c59f0144f861d8c08768a00fff30ccb23f0200
Deane Bliss <dbliss@mi-e1.msk.21-school.ru>
2020-11-28 21:02:59 +0300

    basic cd almost done

--- executor/cd.c
+++ executor/cd.c
@@ -5,24 +5,28 @@ int ft_cd(t_commands *command, t_data *data)
     char *cwd;
     if (command->count_args == 0) // cd only (without args)
     {
-        // обязательно нужно сохранить значение pwd в old_pwd перед запуском функции в переменные окружения!!!
+        set_env_values("OLDPWD", cwd = get_env_values(data->envp, "PWD")); // сохраняем текущий pwd в old_pwd в переменные окружения!!!
+		free(cwd);
 		cwd = get_env_values(data->envp, "HOME");
 		chdir(cwd); // здесь мы попадаем в home
 		free(cwd);
-		data->cwd = getcwd(NULL, 1000); // получаем новое значение для current directory
-        // обязательно нужно сохранить значение текущее значение pwd в переменные окружения
+		cwd = getcwd(NULL, 1000); // получаем новое значение для current directory
+        set_env_values("PWD", cwd); // сохраняем полученное значение в переменных окружения 
     }
     else // cd with path
     {
 	    if (chdir(command->lst) < 0) // если путь, который записан в аргументы невалидный
-		    error_path(command);
+        {
+            error_path(command);
+            return(FAIL);
+        }
 	    else
 	    {
-		    // обязательно нужно сохранить значение pwd в old_pwd перед запуском!
-		    data->cwd = getcwd(NULL, 1000);
-            // обязательно нужно сохранить значение текущее значение pwd в переменные окружения
+            set_env_values("OLDPWD", cwd = get_env_values(data->envp, "PWD")); // сохраняем текущий pwd в old_pwd в переменные окружения!!!
+            free(cwd);
+		    cwd = getcwd(NULL, 1000);
+            set_env_values("PWD", cwd); // сохраняем полученное значение в переменных окружения  
 	    }
     }
-    
 }
 
