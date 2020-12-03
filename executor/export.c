c5bd92e93f86cb89f1fc037fa9b62dc59fb79fc6
Deane Bliss <dbliss@mi-s5.msk.21-school.ru>
2020-12-03 19:54:29 +0300

    sorting and printing export added

--- executor/export.c
+++ executor/export.c
@@ -1,6 +1,87 @@
 #include "../minishell.h"
 
-int ft_export(t_data *data, char *arg)
+void	ft_swap(char *s1, char *s2)
+{
+	char *tmp;
+
+	*tmp = *s1;
+	*s1 = *s2;
+	*s2 = *tmp;
+}
+
+void	ft_sort_list(t_data *data)
+{
+	int i;
+	int diff;
+
+	i = 0;
+	while(data->envp[i + 1])
+	{
+		diff = ft_strcmp(data->envp[i], data->envp[i + 1]);
+		if (diff > 0)
+			ft_swap(data->envp[i], data->envp[i + 1]);
+		i++;
+	}
+}
+
+void ft_print_export(t_data *data)
+{
+    int i;
+    char **split;
+	char *var;
+
+    i = 0;
+    ft_sort_list(data);
+    while (data->envp[i])
+    {
+		var = ft_strchr(data->envp[i], '=');
+        split = ft_split(data->envp[i], '=');
+        ft_putstr_fd("declare -x ", 1);
+        ft_putstr_fd(split[0], 1);
+        if (var)
+        {
+			ft_putchar_fd('=', 1);
+            ft_putchar_fd('"', 1);
+			if (split[1])
+            	ft_putstr_fd(split[1], 1);
+            ft_putchar_fd('"', 1);
+		}
+        ft_putchar_fd('\n', 1);
+        free_tab(split);
+        i++;
+    }
+
+}
+
+static void		print_vars(t_data *data)
+{
+	size_t		index;
+	char		**splited;
+	char		*env;
+
+	index = 0;
+	splited = NULL;
+	sort_vars(data->env);
+	while (data->env[index])
+	{
+		if (!ft_strmatch(data->env[index], "1=1"))
+		{
+			env = ft_strchr(data->env[index], '=');
+			splited = ft_split(data->env[index], '=');
+			if (ft_strncmp(splited[0], "?", 2))
+				ft_printf("declare -x %s", splited[0]);
+			if (env)
+				ft_printf("=\"%s\"\n", env + 1);
+			else
+				ft_printf("\n");
+			ft_clr_darray(splited);
+			splited = NULL;
+		}
+		index++;
+	}
+}
+
+int ft_export_update(t_data *data, char *arg) // это поменять значение уже существующей переменной
 {
 	char	**temp;
 
--- main.c
+++ main.c
@@ -54,6 +54,65 @@ void	delete_env_var(char *var, t_data *data)
 	data->envp = tmp;
 }
 
+void	ft_swap(char **s1, char **s2)
+{
+	char *tmp;
+
+	tmp = *s1;
+	*s1 = *s2;
+	*s2 = tmp;
+}
+
+void	ft_sort_list(t_data *data)
+{
+	int i;
+	int j;
+	int len;
+	int diff;
+
+	i = 0;
+	len = tab_len(data->envp);
+	while(i < len - 1)
+	{
+		j = 0;
+		while (j < len - i - 1)
+		{
+			diff = ft_strcmp(data->envp[j], data->envp[j + 1]);
+			if (diff > 0)
+				ft_swap(&data->envp[j], &data->envp[j + 1]);
+			j++;
+		}
+		i++;
+	}
+}
+
+void ft_print_export(t_data *data)
+{
+    int i;
+    char **split;
+	char *var;
+
+    i = 0;
+    ft_sort_list(data);
+    while (data->envp[i])
+    {
+		var = ft_strchr(data->envp[i], '=');
+        split = ft_split(data->envp[i], '=');
+        ft_putstr_fd("declare -x ", 1);
+        ft_putstr_fd(split[0], 1);
+        if (var)
+        {
+			ft_putchar_fd('=', 1);
+            ft_putchar_fd('"', 1);
+			if (split[1])
+            	ft_putstr_fd(split[1], 1);
+            ft_putchar_fd('"', 1);
+		}
+        ft_putchar_fd('\n', 1);
+        free_tab(split);
+        i++;
+    }
+}
 int main() // testing unset env func
 {
 	t_data *data;
@@ -65,31 +124,32 @@ int main() // testing unset env func
 	data->envp[1] = (char*)malloc(sizeof(char *) * 30 + 1);
 	data->envp[2] = (char*)malloc(sizeof(char *) * 30 + 1);
 	data->envp[3] = (char*)malloc(sizeof(char *) * 30 + 1);
+	data->envp[4] = NULL;
 	
 	int j = 0;
 	int i = 0;
-	char *str1 = "ZSH=/Users/dbliss/.oh-my-zsh"; 
+	char *str1 = "ZSHC=/Users/dbliss/.oh-my-zsh"; 
 	while (str1[j])
 	{
 		data->envp[0][i++] = str1[j++];
 	}
 	i = 0;
 	j = 0;
-	char *str2 = "USER=dbliss";
+	char *str2 = "CSERR=dbliss";
 	while (str2[j])
 	{
 		data->envp[1][i++] = str2[j++];
 	}
 	i = 0;
 	j = 0;
-	char *str3 = "PWD=/Users/dbliss";
+	char *str3 = "abt";
 	while (str3[j])
 	{
 		data->envp[2][i++] = str3[j++];
 	}
 	i = 0;
 	j = 0;
-	char *str4 = "OLDPWD=/Users/dbliss";
+	char *str4 = "abc";
 	while (str4[j])
 	{
 		data->envp[3][i++] = str4[j++];
@@ -99,11 +159,13 @@ int main() // testing unset env func
 	printf("%s\n", data->envp[1]);
 	printf("%s\n", data->envp[2]);
 	printf("%s\n", data->envp[3]);
-	delete_env_var(str, data);
-	printf("\n\n----UNSET VERSION----\n\n");
-	printf("%s\n", data->envp[0]);
-	printf("%s\n", data->envp[1]);
-	printf("%s\n", data->envp[2]);
-	printf("%s\n", data->envp[3]);
+	// delete_env_var(str, data);
+	// printf("\n\n----UNSET VERSION----\n\n");
+	printf("\n\n----PRINT EXPORT----\n\n");
+	ft_print_export(data);
+	// printf("%s\n", data->envp[0]);
+	// printf("%s\n", data->envp[1]);
+	// printf("%s\n", data->envp[2]);
+	// printf("%s\n", data->envp[3]);
 	return (0);
 }
