d5c18511d8151d0d3ae5164380820b2f62f58508
Marvella Onie <monie@il-k3.msk.21-school.ru>
2020-12-18 16:10:51 +0300

    fix redit

--- lexer/analysis/processing_fd.c
+++ lexer/analysis/processing_fd.c
@@ -6,7 +6,7 @@
 /*   By: monie <monie@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 16:24:30 by monie             #+#    #+#             */
-/*   Updated: 2020/12/17 14:27:03 by monie            ###   ########.fr       */
+/*   Updated: 2020/12/18 16:07:55 by monie            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,17 +29,19 @@ void processing_fd(char *str, t_syntax *syntax)
 	if(syntax->ff == 1)
 	{
 		syntax->fd_input = open(str, O_RDWR | O_CREAT | O_TRUNC, S_IWRITE | S_IREAD);
+		//command->redir = 1;
 		//g_error = 125;
 	}
 	if(syntax->mf == 1)
 	{
 		syntax->fd_input = open(str, O_RDWR | O_CREAT | O_APPEND, S_IWRITE | S_IREAD);
+		//command->redir = 1;
 		//g_error = 126;
 	}
 	if(syntax->lf == 1)
 	{
 		syntax->fd_output = open(str, O_RDONLY);
-		//g_error = 127;
+		// command->back_redir = 1;
 	}
 	syntax->f = 0;
 	printf("%d - %d\n", syntax->fd_input, syntax->fd_output);
