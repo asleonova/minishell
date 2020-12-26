d22e6a53101051be660058f99c3829c20a80da3a
Deane Bliss <dbliss@mi-j3.msk.21-school.ru>
2020-12-26 18:35:05 +0300

    pipes one case works

--- executor/pipes.c
+++ executor/pipes.c
@@ -23,87 +23,47 @@ char **ft_list_to_array_2(t_commands *command)
 void pipe_manager(t_commands *command, t_data *data)
 {
     int fd[2];
-            char **argv;
-            char **argv1;
-    pid_t pid1;
-    pid_t pid2;
-   // pid = fork();
-    // if (pid == -1)
-    // {
-    //     // обработка ошибки
-    //     strerror(pid);
-    //     exit(errno);
-    // }
+    char **argv;
+    char **argv1;
+
+    add_path_to_commands(command, data);
+    argv = ft_list_to_array(command);
+    add_path_to_commands(command->next, data);
+    argv1 = ft_list_to_array(command->next);
 
     pipe(fd); // создаем канал для связи
-    pid1 = fork();
+    if (fork() == 0)
     {
-       // if (command->next != NULL)
-        //{
-            //signal(SIGINT, SIG_DFL);
-        //signal(SIGQUIT, SIG_DFL);
-
-        add_path_to_commands(command, data);
-        argv = ft_list_to_array(command);
-            //         printf("LIST ARG: %s\n", command->cmd);
-            // printf("ARGV: %s\n", argv[0]);
+        signal(SIGINT, SIG_DFL);
+        signal(SIGQUIT, SIG_DFL);
+        
         close(fd[0]); // читать из канала не нужно
         dup2(fd[1], 1); // стандартный вывод - в канал.
         close(fd[1]); // fd1 больше не нужен
         // запускаем каоманду
         if(execve(argv[0], argv, data->envp) == -1)
             command_not_found(command);
-        }
-        pid2 = fork();
-    if (pid2 == 0)
-        {
-            add_path_to_commands(command->next, data);
-            argv1 = ft_list_to_array(command->next);
-            // printf("LIST ARG: %s\n", command->next->cmd);
-            // printf("ARGV: %s\n", argv1[0]);
-            close(fd[1]); // fd0 больше не нужен
-            dup2(fd[0], 0); // стандартный ввод - из канала
-            close(fd[0]); // писать в канал не нужно
+        free_tab(argv);
+    }
+    if (fork() == 0)
+    {
+        signal(SIGINT, SIG_DFL);
+        signal(SIGQUIT, SIG_DFL);
+
+        close(fd[1]); // fd0 больше не нужен
+        dup2(fd[0], 0); // стандартный ввод - из канала
+        close(fd[0]); // писать в канал не нужно
             // запускаем вторую команду
-  
-            execve(argv1[0], argv1, data->envp);
-                // command_not_found(command->next);
-        }
+        if(execve(argv1[0], argv1, data->envp) == -1)
+            command_not_found(command);
+        free_tab(argv1);
+    }
     // в родительском процессе закрываем оба конца канала
-        {
-            close(fd[0]);
-        close(fd[1]);
-        wait(&pid1);
-        wait(&pid2);
-      //  wait(NULL);
-        g_error = 0;
-        }
+    signal(SIGINT, SIG_IGN);
+    signal(SIGQUIT, SIG_IGN);
+    close(fd[0]);
+    close(fd[1]);
+    wait(NULL);
+    wait(NULL);
+    g_error = 0;
 }
-
-
-    // else 
-    // {
-    //     close (command->fd[0]);
-    //     close (command->fd[1]);
-    //     wait(&pid);
-    //     g_error = WEXITSTATUS(pid);
-    // }
-    // pid = fork();
-    // if (pid == 0) // other child process
-    // {
-    //     signal(SIGINT, SIG_IGN);
-    //     signal(SIGQUIT, SIG_IGN);
-    //     close(command->fd[1]);
-    //     dup2(command->fd[0], 0); // fd = fd[0] из пайпа, чтобы в след итерации программы уже читалось из fd пайпа
-    //     close(command->fd[0]);
-    //     parse_func(command, data);
-    // }
-    // // else
-    // // {
-    // //     close (command->fd[0]);
-    // //     close (command->fd[1]);
-    // //     wait(&pid);
-    // //     g_error = WEXITSTATUS(pid);
-    // // }
-// }
-
