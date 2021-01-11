5e372e59885fbb8e51518be611ec1b2bbe984584
Marvella Onie <monie@mi-f5.msk.21-school.ru>
2021-01-11 19:39:07 +0300

    7:40

--- signals/signals.c
+++ signals/signals.c
@@ -1,24 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: monie <monie@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/11 19:36:54 by monie             #+#    #+#             */
+/*   Updated: 2021/01/11 19:37:35 by monie            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
-void handler(int signal) 
+void	handler(int signal)
 {
-    if (signal == SIGINT)
-    {
-       write(1, "\b\b  \n", 5);
-	   intro();
-    }
+	if (signal == SIGINT)
+	{
+		write(1, "\b\b  \n", 5);
+		intro();
+	}
 	if (signal == SIGQUIT)
-		write(1, "\b\b  \b\b", 6); 
+		write(1, "\b\b  \b\b", 6);
 }
 
-void signals_default(void)
+void	signals_default(void)
 {
-    signal(SIGINT, SIG_DFL);
+	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
 }
 
-void signals_ignore(void)
+void	signals_ignore(void)
 {
-    signal(SIGINT, SIG_IGN);
+	signal(SIGINT, SIG_IGN);
 	signal(SIGQUIT, SIG_IGN);
 }
