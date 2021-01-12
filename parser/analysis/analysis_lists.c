9e905a624af7baebc4b4f6eb00b11288885b1b1b
Marvella Onie <monie@mi-e5.msk.21-school.ru>
2021-01-12 12:43:50 +0300

    start finish

--- parser/analysis/analysis_lists.c
+++ parser/analysis/analysis_lists.c
@@ -6,51 +6,13 @@
 /*   By: monie <monie@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/26 21:10:50 by monie             #+#    #+#             */
-/*   Updated: 2021/01/11 19:05:22 by monie            ###   ########.fr       */
+/*   Updated: 2021/01/12 12:41:19 by monie            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-int		analysis_zero(t_var *var, t_commands *cmd)
-{
-	t_commands *tmp;
-
-	if (cmd->end)
-	{
-		if (var->list->next)
-		{
-			cmd_initialization(tmp = malloc(sizeof(t_commands)));
-			tmp->prev = cmd;
-			cmd->next = tmp;
-			cmd = tmp;
-		}
-		else
-			return (1);
-	}
-	return (0);
-}
-
-int		analysis_one(t_var *var, t_commands *cmd, char ***env)
-{
-	if (distribution(var->list->content, var, cmd, 0))
-	{
-		if (var->list->content[0] == '>' || var->list->content[0] == '<')
-		{
-			syntax_error();
-			return (1);
-		}
-		if (var->r && cmd->fd_error != 1)
-			processing_fd(var, cmd);
-		if (var->q == 2)
-			parsing_env_quote(var, *env, &var->list->content);
-		if (analysis_zero(var, cmd))
-			return (1);
-	}
-	return (0);
-}
-
-void	analysis_tho(t_var *var, t_commands *cmd, char ***env)
+void	analysis_one(t_var *var, t_commands *cmd, char ***env)
 {
 	if (var->q)
 		quote_cut(var, &var->list->content, 0);
@@ -62,41 +24,60 @@ void	analysis_tho(t_var *var, t_commands *cmd, char ***env)
 		write_argv(var, cmd);
 }
 
-int		analysis_three(t_var *var, t_commands *cmd, t_data *data, int flag)
+void	analysis_export(t_var *var, t_commands *cmd, t_data *data, int flag)
 {
+	if (!ft_strcmp(cmd->cmd, "export") && var->list->next == NULL)
+		{
+			cmd->count_args = ft_lstsize(cmd->arg_lst);
+			if (cmd->count_args == 0)
+			{	
+				ft_print_export(data);	
+				flag = 1;
+			}
+		}
 	if (!ft_strcmp(cmd->cmd, "export") && flag == 0)
 	{
 		while (cmd->arg_lst)
 			ft_export(data, cmd);
 	}
-	if (var->list->next != NULL)
-		var->list = var->list->next;
-	else
-		return (1);
-	return (0);
 }
 
 void	analysis_lists(t_var *var, t_commands *cmd, t_data *data, char ***env)
 {
+	t_commands *tmp;
 	int flag;
 
+	tmp = NULL;
 	flag = 0;
 	cmd_initialization(cmd);
 	while (var->list)
 	{
-		if (analysis_one(var, cmd, env))
-			break ;
-		analysis_tho(var, cmd, env);
-		if (!ft_strcmp(cmd->cmd, "export") && var->list->next == NULL)
+		if (distribution(var->list->content, var, cmd, 0))
 		{
-			cmd->count_args = ft_lstsize(cmd->arg_lst);
-			if (cmd->count_args == 0)
+			if (var->list->content[0] == '>' || var->list->content[0] == '<')
+				syntax_error();
+			if (var->r && cmd->fd_error != 1)
+				processing_fd(var, cmd);
+			if (var->q == 2)
+				parsing_env_quote(var, *env, &var->list->content);
+			if (cmd->end)
 			{
-				ft_print_export(data);
-				flag = 1;
+				if (var->list->next)
+				{
+					cmd_initialization(tmp = malloc(sizeof(t_commands)));
+					tmp->prev = cmd;
+					cmd->next = tmp;
+					cmd = tmp;
+				}
+				else
+					break ;
 			}
 		}
-		if (analysis_three(var, cmd, data, flag))
+		analysis_one(var, cmd, env);
+		analysis_export(var, cmd, data, flag);
+		if (var->list->next != NULL)
+			var->list = var->list->next;
+		else
 			break ;
 	}
 }
